d794bb5f0b4d2816b0ba9e593f7a8d4fb9d33c6d
Sayo <sayo_folabi@hotmail.com>
2017-03-04 22:45:29 +0000

    Hotelshaven android Hello World

---
+++ app/src/main/java/com/haven/hotels/hotelshaven/HHMainActivity.java
@@ -0,0 +1,13 @@
+package com.haven.hotels.hotelshaven;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HHMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hhmain);
+    }
+}
